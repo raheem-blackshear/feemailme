8c47faec4853fc30524046442f2e8770f468001d
Allison <Allison@atebitbyte.com>
2017-11-04 00:14:03 -0700

    modified index and route handler

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-// Create a new express application
+/* Create an Express application */
 
 /* Import Express Library */
-// ES 2015 Module import (below) isn't supported:
-// import express from 'express';
-// support with .mjs file? > node --experimental-modules index.mjs
+// ES 2015 Module imports isn't supported so we can't use import syntax
 const express = require('express');
 
 // Create an application that represents a running express app
-// This will listen for incoming requests on the Node side
+// This listens for incoming requests on the Node side
 // All inbound requests are registered with app
 const app = express();
 
 // Create a route handler associated with the app
 app.get('/', (req, res) => {
-  res.send({hi: 'there'});
+  res.send({goodbye: 'world'});
 });
 
-// Tell the app to listen on port 5000 (localhost:5000)
-// app.listen(5000);
-// If heroku assigns an environment variable for the port, use it; else, use 5000.
+/* Tell the app which port it must listen to */
+// If Heroku assigns an environment variable for the port, use it; else, use 5000.
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
-// Specify the Node environment we want to use
